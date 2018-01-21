4871e75f4539f4bba4f0b2b106a70b01b8b87146
Bogdan Bondaruk <zikbert77@gmail.com>
2018-01-21 17:08:25 +0200

    Add documentation for routes

--- app/config/routes.php
+++ app/config/routes.php
@@ -1,6 +1,11 @@
 <?php
 
+/**
+ * Set route as 'path' => 'NameController/methodAction'
+ * 
+ * Route with parameters 'path/([0-9]+)/([a-zA-Z]+)' => 'NameController/methodAction/$1/$2'
+*/
+
 return [
-    'admin' => 'Admin/index',
     '' => 'Default/index',
 ];
