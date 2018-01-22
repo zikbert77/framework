05125428859ed031d1e169c9904fa522244ad389
Bogdan Bondaruk <david.b@advantagemedia.agency>
2018-01-22 17:15:26 +0200

    clear

--- app/controllers/DefaultController.php
+++ app/controllers/DefaultController.php
@@ -3,13 +3,10 @@
 namespace app\controllers;
 
 use app\Controller;
-use app\models\Test;
 
 class DefaultController extends Controller
 {
     public function actionIndex(){
-
-        Test::test();
 
         return $this->render('index.php');
     }
