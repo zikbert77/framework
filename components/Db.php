e71b872ee4118d9fcae27fe6020c606e41d806c1
Bogdan Bondaruk <david.b@advantagemedia.agency>
2018-01-22 16:23:05 +0200

    Add probels

--- components/Db.php
+++ components/Db.php
@@ -6,20 +6,25 @@ use PDO;
 use PDOException;
 
 class Db {
+
     public static function getConnection(){
+
         $paramsPath = ROOT . '/app/config/db_params.php';
         $params = include($paramsPath);
+
         $dsn = sprintf( "mysql:host=%s;dbname=%s;charset=utf8", $params['host'], $params['db_name']);
         $opt = [
             PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
             PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
             PDO::ATTR_EMULATE_PREPARES   => false,
         ];
+
         try {
             $pdo = new PDO($dsn, $params['user'], $params['password'], $opt);
         } catch (PDOException $e) {
             die('Connecting failure: ' . $e->getMessage());
         }
+
         return $pdo;
     }
 }
