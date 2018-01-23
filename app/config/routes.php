b7c5386e6edfe8f0399a29a4dbf32c6671706a3a
Bogdan Bondaruk <david.b@advantagemedia.agency>
2018-01-23 10:57:23 +0200

    Add speed

--- app/config/routes.php
+++ app/config/routes.php
@@ -1,18 +1,19 @@
 <?php
 
-/**
- * Set route as
- * 'name' => [
- *          'defaults' => 'NameController/methodAction',
- *          'path' => ''
- *          ]
- *
- * Route with parameters
- * name' => [
- *          'defaults' => 'NameController/methodAction/$1/$2',
- *          'path' => 'path/([0-9]+)/([a-zA-Z]+)'
- *          ]
-*/
+
+ /** Set route as
+  'name' => [
+           'defaults' => 'NameController/methodAction',
+           'path' => ''
+           ]
+
+  Route with parameters
+  name' => [
+           'defaults' => 'NameController/methodAction/$1/$2',
+           'path' => 'path/([0-9]+)/([a-zA-Z]+)'
+           ]
+ */
+
 
 return [
     'name' => [
