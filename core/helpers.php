fbd15ac7249552dcbcbe203adc9e3188b934071c
Bogdan Bondaruk <david.b@advantagemedia.agency>
2018-01-23 10:52:10 +0200

    Add dump and path helper functions

--- core/helpers.php
+++ core/helpers.php
@@ -4,6 +4,23 @@
  * Write your own functions and use it anywhere in your beautiful code.
 */
 
-function example() {
-    return print 'Example helper function';
+function dump($var)
+{
+    echo '<pre style="background-color: black; color: darkorange; padding: 5px;">';
+    print_r($var);
+    echo '</pre>';
+}
+
+function path($routeName)
+{
+    try {
+        $routes = include(ROOT . '/app/config/routes.php');
+
+        if (array_key_exists($routeName, $routes))
+            return '/' . $routes[$routeName]['path'];
+
+        throw new Exception(sprintf('Route %s not exist', $routeName));
+    } catch (Exception $e){
+        return print_r($e->getMessage());
+    }
 }
