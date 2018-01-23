0a2badca28ee1abbc5c356bc8b4ddb8331fb2ddf
Bogdan Bondaruk <david.b@advantagemedia.agency>
2018-01-23 12:40:19 +0200

    Add prototype of path function

--- core/helpers.php
+++ core/helpers.php
@@ -11,15 +11,40 @@ function dump($var)
     echo '</pre>';
 }
 
-function path($routeName)
+/**
+ * @return string
+ *
+ * Return formatting path with $optionArray parameters if it defined
+*/
+function path($routeName, $optionArray = []) : string
 {
     try {
         $routes = include(ROOT . '/app/config/routes.php');
 
         if (array_key_exists($routeName, $routes))
-            return '/' . $routes[$routeName]['path'];
+            $path = $routes[$routeName]['path'];
+        else
+            throw new Exception(sprintf('Route %s not exist', $routeName));
 
-        throw new Exception(sprintf('Route %s not exist', $routeName));
+        if (!empty($optionArray)){
+            $replacement = '';
+            foreach ($optionArray as $item) {
+                $replacement .= '/' . $item;
+            }
+
+//            if (preg_match("~{$path}~", $replacement)){
+//                preg_replace("~{$path}~", $replacement, $path);
+//                dump('preg ok');
+//            }
+
+            $explode = explode('/([', $path);
+
+            return '/' . $explode[0] . $replacement;
+        }
+
+        return '/' . $path;
+
+
     } catch (Exception $e){
         return print_r($e->getMessage());
     }
