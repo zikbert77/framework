63f36d25faa4b7e05b3096d3b0e25537732f2667
Bogdan Bondaruk <david.b@advantagemedia.agency>
2018-02-23 10:33:37 +0200

    logger improve

--- components/Logger.php
+++ components/Logger.php
@@ -9,9 +9,17 @@ class Logger
 
     /**
      * @param bool|string $msg
+     * @param bool|string $file
      */
-    public static function log($msg = false)
+    public static function log($msg = false, $file = false)
     {
+
+        if(is_bool($file)) {
+            self::$logFile = 'logs/' . date("Y-M-D") . '.txt';
+        } elseif (is_string($file)){
+            self::$logFile = 'logs/' . $file . '.txt';
+        }
+
         $msg = '[ ' . date('Y-m-d h:i:s') . ' ] ' . $msg . "\n";
 
         file_put_contents(self::$logFile, $msg, FILE_APPEND);
