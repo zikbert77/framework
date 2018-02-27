317ef6e3c052cd7ca8d388ca26c5cfdd430a45f4
Bogdan Bondaruk <david.b@advantagemedia.agency>
2018-02-27 13:37:52 +0200

    Add bootstrap template

--- views/index.php
+++ views/index.php
@@ -1,4 +1,31 @@
-<link rel="shortcut icon" type="image/x-icon" href="favicon.ico"/>
-<h1>Welcome to my framework</h1>
-<hr>
-<span>Let`s code</span>
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+    <!--Bootstrap-->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
+
+    <link rel="shortcut icon" type="image/x-icon" href="favicon.ico"/>
+
+    <title>Framework</title>
+</head>
+<body>
+
+    <div class="container">
+        <h1>Welcome to my framework</h1>
+        <hr>
+        <span>Let`s code</span>
+    </div>
+
+    <!--Bootstrap-->
+    <script
+            src="https://code.jquery.com/jquery-3.3.1.min.js"
+            integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8="
+            crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+</body>
+</html>
