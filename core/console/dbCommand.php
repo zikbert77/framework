7e212efff036b535a898260dbcedd30d4b90b7e4
Your Name <david.b@advantagemedia.agency>
2018-04-28 13:52:19 +0300

    Fix console output

--- core/console/dbCommand.php
+++ core/console/dbCommand.php
@@ -8,7 +8,7 @@ class dbCommand
 {
     public function init()
     {
-        echo "Start importing database...\n";
+        echo "Start importing database...";
 
         if(file_exists(ROOT . 'framework.sql')){
             $pdo = Db::getConnection();
@@ -18,25 +18,20 @@ class dbCommand
 
             foreach ($lines as $line)
             {
-                // Skip it if it's a comment
                 if (substr($line, 0, 2) == '--' || $line == '')
                     continue;
 
-                // Add this line to the current segment
                 $templine .= $line;
 
-                // If it has a semicolon at the end, it's the end of the query
                 if (substr(trim($line), -1, 1) == ';')
                 {
-                    // Perform the query
                     $pdo->query($templine);
-                    // Reset temp variable to empty
                     $templine = '';
                 }
             }
-            echo "\nTables imported successfully!";
+            echo "\nTables imported successfully!\n";
         } else {
-            echo "\nFile with database does not exists!";
+            echo "\nFile with database does not exists!\n";
         }
         return;
     }
